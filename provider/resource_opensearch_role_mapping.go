cc4f75123507e50efe51852cba51826be2fbda5e
Eric Vallee <eric_vallee@webificservices.com>
2022-09-18 03:19:27 -0400

    feat: INFRA-86 Implementation for role mappings

--- provider/resource_opensearch_role_mapping.go
+++ provider/resource_opensearch_role_mapping.go
@@ -1,6 +1,9 @@
 package provider
 
 import (
+	"errors"
+	"fmt"
+	
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
@@ -54,18 +57,94 @@ func resourceOpensearchRoleMapping() *schema.Resource {
 	}
 }
 
+func roleMappingSchemaToModel(d *schema.ResourceData) RoleMappingModel {
+	model := RoleMappingModel{
+		Role: "", 
+		BackendRoles: []string{}, 
+		Hosts: []string{}, 
+		Users: []string{},
+	}
+
+	role, _ := d.GetOk("role")
+	model.Role = role.(string)
+
+	backendRoles, backendRolesExist := d.GetOk("backend_roles")
+	if backendRolesExist {
+		for _, val := range (backendRoles.(*schema.Set)).List() {
+			role := val.(string)
+			model.BackendRoles = append(model.BackendRoles, role)
+		}
+	}
+
+	hosts, hostsExist := d.GetOk("hosts")
+	if hostsExist {
+		for _, val := range (hosts.(*schema.Set)).List() {
+			host := val.(string)
+			model.Hosts = append(model.Hosts, host)
+		}
+	}
+
+	users, usersExist := d.GetOk("users")
+	if usersExist {
+		for _, val := range (users.(*schema.Set)).List() {
+			user := val.(string)
+			model.Users = append(model.Users, user)
+		}
+	}
+
+	return model
+}
+
 func resourceOpensearchRoleMappingCreate(d *schema.ResourceData, meta interface{}) error {
-	return nil
+	cli := meta.(OpensearchClient)
+	roleMapping := roleMappingSchemaToModel(d)
+
+	err := cli.GetRequestContext().UpsertRoleMapping(roleMapping)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error creating role mapping for role '%s': %s", roleMapping.Role, err.Error()))
+	}
+
+	d.SetId(roleMapping.Role)
+	return resourceOpensearchRoleMappingRead(d, meta)
 }
 
 func resourceOpensearchRoleMappingUpdate(d *schema.ResourceData, meta interface{}) error {
-	return nil
+	cli := meta.(OpensearchClient)
+	roleMapping := roleMappingSchemaToModel(d)
+
+	err := cli.GetRequestContext().UpsertRoleMapping(roleMapping)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error updating role mapping for role '%s': %s", roleMapping.Role, err.Error()))
+	}
+
+	return resourceOpensearchRoleMappingRead(d, meta)
 }
 
 func resourceOpensearchRoleMappingRead(d *schema.ResourceData, meta interface{}) error {
+	cli := meta.(OpensearchClient)
+	role := d.Id()
+
+	roleMapping, err := cli.GetRequestContext().GetRoleMapping(role)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error retrieving role mapping for role '%s': %s", role, err.Error()))
+	}
+
+	d.Set("role", role)
+	d.Set("backend_roles", roleMapping.BackendRoles)
+	d.Set("hosts", roleMapping.Hosts)
+	d.Set("users", roleMapping.Users)
+
 	return nil
 }
 
 func resourceOpensearchRoleMappingDelete(d *schema.ResourceData, meta interface{}) error {
+	cli := meta.(OpensearchClient)
+	role := d.Id()
+
+	err := cli.GetRequestContext().DeleteRoleMapping(role)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error deleting role mapping for role '%s': %s", role, err.Error()))
+	}
+
 	return nil
 }
